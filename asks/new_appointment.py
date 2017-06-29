f19d32218ec9d5cd396af64de439f96c30373aea
Vadim Sokoltsov <vforvad@gmail.com>
2017-06-29 12:36:25 +0300

    improve workflow for creation of the appointment

--- asks/new_appointment.py
+++ asks/new_appointment.py
@@ -2,6 +2,8 @@ from flask import render_template
 from flask_ask import question, statement
 import ipdb
 from . import ask, session
+from models import Appointment
+from datetime import datetime
 
 @ask.intent("CuragoAppointmentIntent")
 def create_appointment():
@@ -10,27 +12,38 @@ def create_appointment():
 
 @ask.intent("CuragoAppointmentDateIntent", convert={'begin_date': 'date'})
 def appointment_date(begin_date):
-    # session.attributes['begin_date'] = begin_date
+    session.attributes['begin_date'] = str(begin_date)
     qs = render_template('time')
     return question(qs)
 
 @ask.intent("CuragoAppointmentTimeIntent", convert={'begin_time': 'time'})
 def appointment_time(begin_time):
-    # session.attributes['begin_time'] = begin_time
+    session.attributes['begin_time'] = str(begin_time)
     msg = render_template('end_date')
     return question(msg)
 
 @ask.intent("CuragoAppointmentEndDateIntent", convert={'end_date': 'date'})
 def appointment_end_date(end_date):
-    # session.attributes['end_date'] = end_date
+    session.attributes['end_date'] = str(end_date)
     msg = render_template('end_time')
     return question(msg)
 
 @ask.intent("CuragoAppointmentEndTimeIntent", convert={'end_time': 'time'})
 def appointment_end_time(end_time):
-    # session.attributes['end_time'] = end_time
-    msg = render_template('created_succesfully')
+    session.attributes['end_time'] = str(end_time)
+    full_begin_date = session.attributes['begin_date'] + ' ' + session.attributes['begin_time']
+    full_end_date = session.attributes['end_date'] + ' ' + session.attributes['end_time']
+    full_begin_datetime = datetime.strptime(full_begin_date, '%Y-%m-%d %H:%M:%S')
+    full_end_datetime = datetime.strptime(full_end_date, '%Y-%m-%d %H:%M:%S')
+    appointment = Appointment(begin_date=full_begin_datetime, end_date=full_end_datetime)
+    appointment.save()
+    msg = render_template('created_succesfully',
+                          begin_date=appointment.begin_date.date(),
+                          begin_time=appointment.begin_date.time(),
+                          end_date=appointment.end_date.date(),
+                          end_time=appointment.end_date.time())
     return statement(msg)
+
 
 @ask.intent("CuragoAppointmentWithBeginDateAndTimeIntent",
             convert={'begin_date': 'date', 'begin_time': 'time'})
@@ -86,6 +99,6 @@ def add_new_object(ownerName, collection_owner_name, object_start_date,
         return question('date')
     if object_start_time is None:
         return question('time')
-    ipdb.set_trace()
-    msg = render_template('created_succesfully')
-    return statement(msg)
+    # ipdb.set_trace()
+    # msg = render_template('created_succesfully')
+    # return statement(msg)
--- manage.py
+++ manage.py
@@ -2,10 +2,42 @@ from flask import Flask
 from flask_script import Manager, Server
 from models import db
 from asks import blueprint
+import datetime
+import re
 
 app = Flask(__name__)
 app.register_blueprint(blueprint)
 manager = Manager(app)
+
+@app.template_filter()
+def humanize_date(dt):
+    ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
+    month_and_day_of_week = dt.strftime('%A %B')
+    day_of_month = ordinal(dt.day)
+    year = dt.year if dt.year != datetime.datetime.now().year else ""
+    formatted_date = "{} {} {}".format(month_and_day_of_week, day_of_month, year)
+    formatted_date = re.sub('\s+', ' ', formatted_date)
+    return formatted_date
+
+
+@app.template_filter()
+def humanize_time(dt):
+    morning_threshold = 12
+    afternoon_threshold = 17
+    evening_threshold = 20
+    hour_24 = dt.hour
+    if hour_24 < morning_threshold:
+        period_of_day = "in the morning"
+    elif hour_24 < afternoon_threshold:
+        period_of_day = "in the afternoon"
+    elif hour_24 < evening_threshold:
+        period_of_day = "in the evening"
+    else:
+        period_of_day = " at night"
+    the_time = dt.strftime('%I:%M')
+    formatted_time = "{} {}".format(the_time, period_of_day)
+    return formatted_time
+
 
 manager.add_command("runserver", Server(
     use_debugger=True,
