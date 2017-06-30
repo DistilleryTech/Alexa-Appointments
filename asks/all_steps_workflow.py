e6de9785f548ce6f5ab00779a54016b8e777a25e
Vadim Sokoltsov <vforvad@gmail.com>
2017-06-30 12:30:48 +0300

    move full steps workflow into seperated file

--- asks/all_steps_workflow.py
+++ asks/all_steps_workflow.py
@@ -1,2 +1,31 @@
 from . import (ask, session, render_template, question, statement,
                Appointment, AppointmentForm)
+
+@ask.intent("CuragoAppointmentIntent")
+def create_appointment():
+    msg = render_template('date')
+    return question(msg)
+
+@ask.intent("CuragoAppointmentDateIntent", convert={'begin_date': 'date'})
+def appointment_date(begin_date):
+    session.attributes['begin_date'] = str(begin_date)
+    qs = render_template('time')
+    return question(qs)
+
+@ask.intent("CuragoAppointmentTimeIntent", convert={'begin_time': 'time'})
+def appointment_time(begin_time):
+    session.attributes['begin_time'] = str(begin_time)
+    return question(msg)
+
+@ask.intent("CuragoAppointmentEndDateIntent", convert={'end_date': 'date'})
+def appointment_end_date(end_date):
+    session.attributes['end_date'] = str(end_date)
+    msg = render_template('end_time')
+    return question(msg)
+
+@ask.intent("CuragoAppointmentEndTimeIntent", convert={'end_time': 'time'})
+def appointment_end_time(end_time):
+    session.attributes['end_time'] = str(end_time)
+    form = AppointmentForm(session.attributes)
+    form.submit()
+    render_result(form)
