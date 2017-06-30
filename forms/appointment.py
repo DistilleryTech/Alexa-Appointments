097a788f0ec6279d502baee1274f8998af83f58c
Vadim Sokoltsov <vforvad@gmail.com>
2017-06-30 12:57:44 +0300

    add comments to model and form-object

--- forms/appointment.py
+++ forms/appointment.py
@@ -2,8 +2,13 @@ from . import Appointment
 from datetime import datetime
 
 class AppointmentForm:
+    """ Form object for creating new appointment """
 
     def __init__(self, attributes):
+        """
+        Create new instance of AppointmentForm;
+        Set begin and end dates values
+        """
 
         formatted_begin_date = '{} {}'.format(
                 attributes['begin_date'],
@@ -19,8 +24,11 @@ class AppointmentForm:
         self.end_date = datetime.strptime(
                 formatted_end_date, '%Y-%m-%d %H:%M:%S'
         )
+        return self
 
     def submit(self):
+        """ Create and return new appointment """
+
         self.appointment = Appointment(
                 begin_date=self.begin_date,
                 end_date=self.end_date
--- models/appointment.py
+++ models/appointment.py
@@ -1,6 +1,8 @@
 from . import db
 
 class Appointment(db.Document):
+    """ Appointment object presentation """
+
     text = db.StringField(max_length=255)
     begin_date = db.DateTimeField()
     end_date = db.DateTimeField()
